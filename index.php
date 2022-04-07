0a25f5d35e47622d37fdb195f8e2aa49142dae41
ar2rooo <100130848+ar2rooo@users.noreply.github.com>
2022-04-07 21:27:02 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body><center>
+ <br><br><br>
+ <?php echo '<p><b>TST Environment</b></p>'; 
+ 
+ $image_url='https://nordcloud.com/wp-content/uploads/2022/01/nordcloud-ibm-logo.svg';
+ 
+ ?> 
+<br>
+<img style="width: 20%" src="<?php echo $image_url;?>">
+<br><br><br>
+
 <?php
+$servername = "10.0.2.4";
+$username = "azureadmin";
+$password = "ar2rooo1234!";
+$db = "db_test";
 
-require __DIR__ . '/vendor/autoload.php';
-use Carbon\Carbon;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully with " . $db;
+?>
 
 
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
+ </center></body>
+</html>
 
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
 
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+
